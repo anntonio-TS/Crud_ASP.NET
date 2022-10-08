b27422c5b0ceb5b2cfb64bf524424ba738ccc908
unknown <atojins@miumg.edu.gt>
2022-10-08 12:08:48 -0600

    primer commit

---
+++ ConexionBD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConexionBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
